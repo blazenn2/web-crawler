691175b9722dbd9e0884cc2e9a634a41e09808aa
blazenn2 <hamzanawab.bs@gmail.com>
2023-04-26 19:59:04 +0500

    Was doing wrong the whole time ... now scrapping data directly from html tags

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,6 @@ app.post("/get_tickets", async (req: Request, res: Response) => {
         await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 });
         console.log('Navigation finished');
 
-        const flightDom: string[] = await page.$$eval('.flight-item', elements => elements.map(element => element.textContent)) as string[];
         let airlinesDom: string = await page.$eval('#toggleAirlines', element => element.textContent?.trim()) as string;
         const airLineObject: any = {};
         if (!airlinesDom) return res.status(200).json({ success: true, message: "No tickets available!" });
@@ -54,79 +53,142 @@ app.post("/get_tickets", async (req: Request, res: Response) => {
             airlinesDom = airlinesDom.replace(airlineStringToDelete, "");
         };
         await page.screenshot({ path: 'ss.png' });
+        const result: any = await page.$$eval('.flight-item', elements => elements.map(element => {
+            // Departure details
+
+            // Scrapping airline information
+            const cardHeaderElement: Element = (element.querySelector(".card-header")) as Element;
+            const airline: string[] = cardHeaderElement?.querySelector("h6")?.textContent?.split(",") || ["null", "null"];
+            const isRefundable: string = cardHeaderElement?.querySelector("span")?.textContent || "";
+
+            // Scrapping timing information
+            const tripInfo: Element[] = [...element.querySelectorAll(".col-sm-2")];
+            const startTripDate: Element[] = [...tripInfo[1].querySelectorAll("small")];
+            const startTripTime: string = tripInfo[1].querySelector("span")?.textContent || "";
+            const endTripInfo: Element[] = [...tripInfo[2].querySelectorAll("small")];
+            const endTripTime: string = tripInfo[2].querySelector("span")?.textContent || "";
+
+            // Scrapping stops information
+            const stopsInfoCard: Element = (element.querySelector(".col-sm-6")) as Element;
+            const stopsInfo: string = stopsInfoCard?.querySelector(".text-primary")?.textContent || "";
+            const upperContent: Element = (stopsInfoCard?.querySelector(".upper-content")) as Element;
+            const stopLayoverTime: Element[] = [...upperContent.childNodes] as Element[];
+            const lowerContent: Element = (stopsInfoCard?.querySelector(".lower-content")) as Element;
+            const stopLocations: Element[] = [...lowerContent.childNodes] as Element[];
+
+
+            // Destination Trip Information
+            const destinationObject: any = {
+                airlineName: airline[0],
+                fromLocation: startTripDate[1].textContent || "",
+                toLocation: endTripInfo[1].textContent || "",
+                startDate: startTripDate[0].textContent || "",
+                startTime: startTripTime,
+                endDate: endTripInfo[0].textContent || "",
+                endTime: endTripTime,
+                isRefundable: isRefundable
+            };
+
+            if (stopsInfo) {
+                if (stopsInfo === "Non-Stop") destinationObject["numberOfStops"] = "0";
+                else {
+                    destinationObject["numberOfStops"] = stopsInfo.split(" ")[0].toString();
+                    const stopLocationArray: string[] = stopLocations.map((element: Element) => element.textContent || "");
+                    stopLayoverTime.forEach((element: Element, i: number) => {
+                        const afterLayoverTime = (element as HTMLElement)?.title;
+                        const layoverTime: string = element.textContent || "";
+                        const stop: string = stopLocationArray[i];
+
+                        const startDateTime: string = tripInfo[1].querySelector("span")?.title || "";
+                        const endDateTime: string = tripInfo[2].querySelector("span")?.title || "";
+
+                        destinationObject[`stopNo${i + 1}`] = stop;
+                        destinationObject[`${stop}LayoverTime`] = layoverTime;
+                        destinationObject[`${stop}LeaveTime`] = afterLayoverTime;
+                    });
+                };
+            };
+
+
+            return destinationObject;
+            // tripObject.airlineName = airline[0];
+            // tripObject.fromLocation = [...tripTimeAndStopInfo[0].querySelectorAll("small")][1].textContent || "";
+            // return tripTimeAndStopInfo
+
+        })) as any;
         console.log("Data scrapped successfully!");
 
-        const cleanedDataArray: string[][] = (flightDom.map(element => element.split("  ").filter(value => value !== "")));
-        cleanedDataArray.forEach(data => {
-            // First airway information
-            const firstAirwayInfo: string[] = data[0].split(",");
-            const firstStartTimeInfo: string[] = data[1].split(" ");
-            const firstCostInfo: string[] = data[data.length - 1].split(" ");
-            const firstEndTimeInfo: string[] = data[2].split(" ");
-            const firstEndTimeInfoLength: number = firstEndTimeInfo.length;
-            const firstAirwayObject: any = {
-                airline: firstAirwayInfo[0],
-                price: `${firstCostInfo[1]} ${firstCostInfo[2]}`,
-                from: firstStartTimeInfo[3],
-                to: firstEndTimeInfo[firstEndTimeInfoLength - 1],
-                startDateAndTime: `${firstStartTimeInfo[2]} ${firstStartTimeInfo[0]} ${firstStartTimeInfo[1]}`,
-                endDateAndTime: `${firstEndTimeInfo[firstEndTimeInfoLength - 2]} ${firstEndTimeInfo[firstEndTimeInfoLength - 4]} ${firstEndTimeInfo[firstEndTimeInfoLength - 3]}`,
-                isTicketRefundable: firstAirwayInfo[1].split(" ")[firstAirwayInfo[1].split(" ").length - 1],
-            };
-            if (data[2].split(' ')[0] === "Non-Stop") {
-                firstAirwayObject.numberOfStops = "0";
-            } else {
-                firstAirwayObject.numberOfStops = data[2].split(' ')[1];
-                const layoverPlaces: string = data[2].split(' ')[0];
-                let layoverTime: string = firstStartTimeInfo.filter((_, i) => i > 3).join('');
-                for (let i: number = 1; i <= Number(firstAirwayObject.numberOfStops); i++) {
-                    const subStringLayoverTime: string[] = layoverTime.substring(0, layoverTime.indexOf('M') + 1).split('');
-                    subStringLayoverTime.splice(subStringLayoverTime.indexOf('H') + 1, 0, " ");
-                    layoverTime = layoverTime.replace(subStringLayoverTime.join(''), "");
-                    firstAirwayObject[`stop${i}Location`] = layoverPlaces.split('').splice(((i - 1) * 3), (i * 3)).join('');
-                    firstAirwayObject[`stop${i}Layover`] = subStringLayoverTime.join('');
-                };
-            }
-            airLineObject[`${firstAirwayInfo[0].split(" ").join("_")}`].push(firstAirwayObject);
+        // const cleanedDataArray: string[][] = (flightDom.map(element => element.split("  ").filter(value => value !== "")));
+        // cleanedDataArray.forEach(data => {
+        //     // First airway information
+        //     const firstAirwayInfo: string[] = data[0].split(",");
+        //     const firstStartTimeInfo: string[] = data[1].split(" ");
+        //     const firstCostInfo: string[] = data[data.length - 1].split(" ");
+        //     const firstEndTimeInfo: string[] = data[2].split(" ");
+        //     const firstEndTimeInfoLength: number = firstEndTimeInfo.length;
+        //     const firstAirwayObject: any = {
+        //         airline: firstAirwayInfo[0],
+        //         price: `${firstCostInfo[1]} ${firstCostInfo[2]}`,
+        //         from: firstStartTimeInfo[3],
+        //         to: firstEndTimeInfo[firstEndTimeInfoLength - 1],
+        //         startDateAndTime: `${firstStartTimeInfo[2]} ${firstStartTimeInfo[0]} ${firstStartTimeInfo[1]}`,
+        //         endDateAndTime: `${firstEndTimeInfo[firstEndTimeInfoLength - 2]} ${firstEndTimeInfo[firstEndTimeInfoLength - 4]} ${firstEndTimeInfo[firstEndTimeInfoLength - 3]}`,
+        //         isTicketRefundable: firstAirwayInfo[1].split(" ")[firstAirwayInfo[1].split(" ").length - 1],
+        //     };
+        //     if (data[2].split(' ')[0] === "Non-Stop") {
+        //         firstAirwayObject.numberOfStops = "0";
+        //     } else {
+        //         firstAirwayObject.numberOfStops = data[2].split(' ')[1];
+        //         const layoverPlaces: string = data[2].split(' ')[0];
+        //         let layoverTime: string = firstStartTimeInfo.filter((_, i) => i > 3).join('');
+        //         for (let i: number = 1; i <= Number(firstAirwayObject.numberOfStops); i++) {
+        //             const subStringLayoverTime: string[] = layoverTime.substring(0, layoverTime.indexOf('M') + 1).split('');
+        //             subStringLayoverTime.splice(subStringLayoverTime.indexOf('H') + 1, 0, " ");
+        //             layoverTime = layoverTime.replace(subStringLayoverTime.join(''), "");
+        //             firstAirwayObject[`stop${i}Location`] = layoverPlaces.split('').splice(((i - 1) * 3), (i * 3)).join('');
+        //             firstAirwayObject[`stop${i}Layover`] = subStringLayoverTime.join('');
+        //         };
+        //     }
+        //     airLineObject[`${firstAirwayInfo[0].split(" ").join("_")}`].push(firstAirwayObject);
 
-            // Second airway information
-            const secondAirwayInfo: string[] = firstAirwayInfo[1].trim().split(" ");
-            const secondStartTimeInfo: string[] = data[3].split(" ");
-            const secondEndTimeInfo: string[] = data[data.length - 2].split(" ");
-            const secondEndTimeInfoLength: number = secondEndTimeInfo.length;
-            const secondAirwayObject: any = {
-                airline: secondAirwayInfo.splice(0, secondAirwayInfo.length - 1).join(" "),
-                price: `${firstCostInfo[1]} ${firstCostInfo[2]}`,
-                from: secondStartTimeInfo[3],
-                to: secondEndTimeInfo[secondEndTimeInfoLength - 1],
-                startDateAndTime: `${secondStartTimeInfo[2]} ${secondStartTimeInfo[0]} ${secondStartTimeInfo[1]}`,
-                endDateAndTime: `${secondEndTimeInfo[secondEndTimeInfoLength - 2]} ${secondEndTimeInfo[secondEndTimeInfoLength - 4]} ${secondEndTimeInfo[secondEndTimeInfoLength - 3]}`,
-                isTicketRefundable: firstAirwayInfo[1].split(" ")[firstAirwayInfo[1].split(" ").length - 1],
-            };
-            if (data[4].split(' ')[0] === "Non-Stop") {
-                secondAirwayObject.numberOfStops = "0";
-            } else {
-                secondAirwayObject.numberOfStops = data[4].split(' ')[1];
-                const layoverPlaces: string = data[4].split(' ')[0];
-                let layoverTime: string = secondStartTimeInfo.filter((_, i) => i > 3).join('');
-                for (let i: number = 1; i <= Number(secondAirwayObject.numberOfStops); i++) {
-                    const subStringLayoverTime: string[] = layoverTime.substring(0, layoverTime.indexOf('M') + 1).split('');
-                    subStringLayoverTime.splice(subStringLayoverTime.indexOf('H') + 1, 0, " ");
-                    layoverTime = layoverTime.replace(subStringLayoverTime.join(''), "");
-                    secondAirwayObject[`stop${i}Location`] = layoverPlaces.split('').splice(((i - 1) * 3), (i * 3)).join('');
-                    secondAirwayObject[`stop${i}Layover`] = subStringLayoverTime.join('');
-                };
-            }
-            airLineObject[`${secondAirwayObject.airline.split(" ").join("_")}`].push(secondAirwayObject);
+        //     // Second airway information
+        //     const secondAirwayInfo: string[] = firstAirwayInfo[1].trim().split(" ");
+        //     const secondStartTimeInfo: string[] = data[3].split(" ");
+        //     const secondEndTimeInfo: string[] = data[data.length - 2].split(" ");
+        //     const secondEndTimeInfoLength: number = secondEndTimeInfo.length;
+        //     const secondAirwayObject: any = {
+        //         airline: secondAirwayInfo.splice(0, secondAirwayInfo.length - 1).join(" "),
+        //         price: `${firstCostInfo[1]} ${firstCostInfo[2]}`,
+        //         from: secondStartTimeInfo[3],
+        //         to: secondEndTimeInfo[secondEndTimeInfoLength - 1],
+        //         startDateAndTime: `${secondStartTimeInfo[2]} ${secondStartTimeInfo[0]} ${secondStartTimeInfo[1]}`,
+        //         endDateAndTime: `${secondEndTimeInfo[secondEndTimeInfoLength - 2]} ${secondEndTimeInfo[secondEndTimeInfoLength - 4]} ${secondEndTimeInfo[secondEndTimeInfoLength - 3]}`,
+        //         isTicketRefundable: firstAirwayInfo[1].split(" ")[firstAirwayInfo[1].split(" ").length - 1],
+        //     };
+        //     if (data[4].split(' ')[0] === "Non-Stop") {
+        //         secondAirwayObject.numberOfStops = "0";
+        //     } else {
+        //         secondAirwayObject.numberOfStops = data[4].split(' ')[1];
+        //         const layoverPlaces: string = data[4].split(' ')[0];
+        //         let layoverTime: string = secondStartTimeInfo.filter((_, i) => i > 3).join('');
+        //         for (let i: number = 1; i <= Number(secondAirwayObject.numberOfStops); i++) {
+        //             const subStringLayoverTime: string[] = layoverTime.substring(0, layoverTime.indexOf('M') + 1).split('');
+        //             subStringLayoverTime.splice(subStringLayoverTime.indexOf('H') + 1, 0, " ");
+        //             layoverTime = layoverTime.replace(subStringLayoverTime.join(''), "");
+        //             secondAirwayObject[`stop${i}Location`] = layoverPlaces.split('').splice(((i - 1) * 3), (i * 3)).join('');
+        //             secondAirwayObject[`stop${i}Layover`] = subStringLayoverTime.join('');
+        //         };
+        //     }
+        //     airLineObject[`${secondAirwayObject.airline.split(" ").join("_")}`].push(secondAirwayObject);
 
-        });
+        // });
         console.log("Data cleaned successfully!");
 
 
-        return res.status(200).json({ success: true, totalAirlines: airLineObject, dom: flightDom });
+        return res.status(200).json({ success: true, tripType: "Round Trip", totalAirlines: result, dom: result });
     } catch (err: any) {
         // await browser.close();
-        await page.screenshot({ path: 'ss.png' });
+        // await page.screenshot({ path: 'ss.png' });
         console.log(err);
         res.status(400).json({ success: false, message: err.message });
     } finally {
